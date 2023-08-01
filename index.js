dcdaca8372e38b9f5eb63e210c59d75c94dc3c30
lionel <staricofflionel@hotmail.com>
2023-08-01 10:47:49 -0300

    se agrega funcionalidad para cargar el contador de pokemon atrapados

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const tracker = document.querySelector('.tracker');
-let puntero = document.querySelector('pokebola');
-
+let tracker = document.querySelector('.tracker');
+let puntero = document.querySelector('#pokebola');
+let despaparecer = true;
          
 
 document.body.addEventListener('mousemove',e =>{
@@ -10,15 +10,44 @@ document.body.addEventListener('mousemove',e =>{
 
 
 
-function agregarBotones(){
+function abrirPokebola(){
     let pokemon = document.querySelectorAll("button");
     pokemon.forEach(element => {
         element.addEventListener('click', e=>{
             puntero.src = "img/pokeballOpen.png";
-            alert('hola');
+            tracker.style.height = "150px";
+            tracker.style.width = "150px";  
+            despaparecer = true;
+            cerrarpokebola();
+            sumarPokemon();
+          
     });
    
 });
 }
 
-agregarBotones();
+
+function cerrarpokebola(){
+    let pokemon = document.querySelectorAll("button");
+    pokemon.forEach(element => {
+        element.addEventListener('mouseout', e=>{
+            puntero.src = "img/pokeball.png";
+            tracker.style.height = "50px";
+            tracker.style.width = "50px";  
+            if(despaparecer){
+            element.style.display = "none"  
+            despaparecer = false;
+            }  
+    });
+   
+});
+}
+
+function sumarPokemon(){
+    let contador =parseInt(document.querySelector('span').innerHTML);
+    document.querySelector('span').innerText =  contador+1;
+}
+
+abrirPokebola();
+
+
